28a954cdef525dd89c622c8a70344994bb598e5f
EmmanulH <133886298+Emmanul-Haque@users.noreply.github.com>
2024-11-10 17:11:48 +0530

    Update script.js

--- Bankist/script.js
+++ Bankist/script.js
@@ -87,10 +87,32 @@ displayMovements(account1.movements);
 const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
 
-  labelBalance.textContent = `${balance} €`;
+  labelBalance.textContent = `${balance}€`;
 };
 
 calcDisplayBalance(account1.movements);
+
+// calculate the total incomes, outcome and interest given out by bank
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, deposit) => acc + deposit, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const outcomes = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, withdrew) => acc + withdrew, 0);
+  labelSumOut.textContent = `${Math.abs(outcomes)}€`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter(interest => interest >= 1)
+    .reduce((acc, interest) => acc + interest, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
+
+calcDisplaySummary(account1.movements);
 
 // create username ➡️ Md Emmanul Haque's username = MEH
 const createUsernames = function (accs) {
