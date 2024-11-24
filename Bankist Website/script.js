7f04566aa1b534abc1ac35ce45bd01be65c4b1d0
EmmanulH <133886298+Emmanul-Haque@users.noreply.github.com>
2024-11-24 19:09:32 +0530

    Updated script.js(Added Page navigation smoothly, Tabbed component, Menu fade animation, Implementing Sticky Navigation, Reveal sections)

--- Bankist Website/script.js
+++ Bankist Website/script.js
@@ -1,12 +1,31 @@
 'use strict';
 
 ///////////////////////////////////////
-// Modal window
+// Selecting HTML using JS
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+const tabs = document.querySelectorAll('.operations__tab ');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+const navLinks = document.querySelector('.nav__links');
+
+const navbar = document.querySelector('.nav');
+
+const navbarHeight = navbar.getBoundingClientRect().height; // This will help to get the navbar height dynamically
+const header = document.querySelector('.header');
+
+const allSections = document.querySelectorAll('.section');
+
+///////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -32,10 +51,8 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-// Implementing Smooth Scrolling
-
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+///////////////////////////////////////
+// Implementing Button Smooth Scrolling
 
 btnScrollTo.addEventListener('click', function (e) {
   // To get co-ordinates according to the rectangle (as our webpages looks like rectangle) getBoundingClientRect(). And this method is relative to the visible viewport.
@@ -71,4 +88,99 @@ btnScrollTo.addEventListener('click', function (e) {
 
   // 2nd way: Modern way
   section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+///////////////////////////////////////
+// Page navigation smoothly
+
+/*
+// Using Event Delegations
+1. Add event listener to common parent element
+2. Determine what element originated the event
+*/
+
+navLinks.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // Matching Strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+///////////////////////////////////////
+// Tabbed component
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Gaurd Clause
+  if (!clicked) return;
+
+  // -> First removing the existing active class
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(t => t.classList.remove('operations__content--active'));
+
+  // Active tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.getAttribute('data-tab')}`)
+    .classList.add('operations__content--active');
+});
+
+///////////////////////////////////////
+// Menu fade animation
+const handlerHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav__links').querySelectorAll('.nav__link');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+  }
+};
+
+// Passing "argument" into handler
+navLinks.addEventListener('mouseover', handlerHover.bind(0.5));
+navLinks.addEventListener('mouseout', handlerHover.bind(1));
+
+///////////////////////////////////////
+// Implementing Sticky Navigation
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) navbar.classList.add('sticky');
+  else navbar.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  // rootMargin: '-90px', // This will help the navbar to appear just when the header have 90px left, therfore -90px.
+  rootMargin: `-${navbarHeight}px`, // This will help when we make website responsive for different screen sizes
+});
+headerObserver.observe(header);
+
+///////////////////////////////////////
+// Reveal sections
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionsObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionsObserver.observe(section);
+  section.classList.add('section--hidden');
 });
