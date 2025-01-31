a392364b50ef27ea9d849e151c16ad42dcba9c6b
EmmanulH <133886298+Emmanul-Haque@users.noreply.github.com>
2025-01-31 17:42:58 +0530

    Fixed the bug in Reveal Section

--- Bankist Website/script.js
+++ Bankist Website/script.js
@@ -170,12 +170,12 @@ headerObserver.observe(header);
 ///////////////////////////////////////
 // Reveal sections
 const revealSection = function (entries, observer) {
-  const [entry] = entries;
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
 
-  if (!entry.isIntersecting) return;
-
-  entry.target.classList.remove('section--hidden');
-  observer.unobserve(entry.target);
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+  });
 };
 
 const sectionsObserver = new IntersectionObserver(revealSection, {
